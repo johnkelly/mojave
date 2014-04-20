2842a39a71ed8eaac9103eee53b46ec1a96a6217
John Kelly <mr.johnkelly@live.com>
2014-04-19 21:44:27 -0700

    Can create and delete ingredients from list

--- www/js/controllers/ingredients.js
+++ www/js/controllers/ingredients.js
@@ -1,6 +1,31 @@
-app.controller('IngredientsCtrl', function($scope, $state, Ingredient, Helpers) {
+app.controller('IngredientsCtrl', function($scope, $state, Ingredient, Measurement, Helpers) {
+  $scope.newIngredient = {}
 
   $scope.ingredient_data = Ingredient.get({}, function() {
     $scope.ingredients = $scope.ingredient_data.ingredients;
   });
+
+  $scope.measurement_data = Measurement.get({}, function() {
+    $scope.measurements = $scope.measurement_data.measurements;
+  });
+
+
+  $scope.createIngredient = function() {
+    Ingredient.save(
+      { ingredients: $scope.newIngredient },
+      function(response) {
+        $scope.ingredients.push(response.ingredient);
+        $scope.newIngredient = {};
+      },
+      function(response) {
+        Helpers.ajax_error_handling(response);
+      }
+    );
+  }
+
+  $scope.deleteIngredient = function(ingredient, index) {
+    Ingredient.remove({ id: ingredient.id }, function() {
+      $scope.ingredients.splice(index, 1);
+    })
+  }
 })
---
+++ www/js/models/measurement.js
@@ -0,0 +1,13 @@
+app.factory('Measurement', function ($resource, CurrentUser) {
+  return $resource('http://localhost:5000/measurements/:id',
+    { id: '@id' },
+    {
+      'get':    { method:'GET', headers: CurrentUser.authorization_header() },
+      'save':   { method:'POST', headers: CurrentUser.authorization_header() },
+      'query':  { method:'GET', isArray:true, headers: CurrentUser.authorization_header() },
+      'remove': { method:'DELETE', headers: CurrentUser.authorization_header() },
+      'delete': { method:'DELETE', headers: CurrentUser.authorization_header() },
+      'update': { method:'PATCH', headers: CurrentUser.authorization_header() }
+    });
+});
+
