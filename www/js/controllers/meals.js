82da86ae2acca92107645146d39510cba992c87f
John Kelly <mr.johnkelly@live.com>
2014-04-23 23:20:45 -0700

    Add basic meal page with create, index, destroy

--- www/js/controllers/meals.js
+++ www/js/controllers/meals.js
@@ -1,2 +1,26 @@
-app.controller('MealsCtrl', function($scope, $state, CurrentUser) {
+app.controller('MealsCtrl', function($scope, $state, UserMeal, CurrentUser, Helpers) {
+  $scope.newUserMeal = {}
+
+  $scope.user_meal_data = UserMeal.get({}, function() {
+    $scope.user_meals = $scope.user_meal_data.meals;
+  });
+
+  $scope.createUserMeal = function() {
+    UserMeal.save(
+      { meal: $scope.newUserMeal },
+      function(response) {
+        $scope.user_meals.push(response.users_meal);
+        $scope.newIngredient = {};
+      },
+      function(response) {
+        Helpers.ajax_error_handling(response);
+      }
+      );
+  }
+
+  $scope.deleteUserMeal = function(user_meal) {
+    UserMeal.remove({ id: user_meal.id }, function() {
+      $scope.user_meals.splice($scope.user_meals.indexOf(user_meal), 1);
+    })
+  }
 })
