68356c3b2c3912c4d0c0991924a789041003bb23
John Kelly <mr.johnkelly@live.com>
2014-05-15 19:13:15 -0700

    Hard refresh the entire app to avoid leaking info
    Angular Resource is caching the headers for some reason   even though they have been deleted from local storage & caching is off by default.
    
    location.reload() reloads the whole angular portion of the app which is an ok fix for now (Same behavior as RB)

--- www/js/controllers/app.js
+++ www/js/controllers/app.js
@@ -1,10 +1,10 @@
-app.controller('AppCtrl', function($scope, $state, CurrentUser) {
+app.controller('AppCtrl', function($scope, CurrentUser) {
   $scope.first_name = function() {
     return CurrentUser.first_name();
   }
 
   $scope.signOut = function() {
     CurrentUser.delete_all();
-    $state.go('welcome');
+    location.reload()
   }
 })
