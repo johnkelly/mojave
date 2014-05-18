ced7e7a07f9b9405665c063164370acd237d75c0
John Kelly <mr.johnkelly@live.com>
2014-05-18 16:36:24 -0700

    Fix the pass button on menu page

--- www/js/controllers/meals.js
+++ www/js/controllers/meals.js
@@ -6,13 +6,14 @@ app.controller('MealsCtrl', function($scope, $state, AvailableMeal, Meal, UserMe
     $scope.content_loaded = true;
     Helpers.hide_loading();
     $scope.available_meals = Helpers.shuffle_array($scope.available_meal_data.available_meals);
-    $scope.current_meal_index = 0
-    $scope.available_meal = $scope.available_meals[$scope.current_meal_index]
+    $scope.current_meal_index = 0;
+    $scope.available_meal = $scope.available_meals[$scope.current_meal_index];
   });
 
   $scope.nextAvailableMeal = function() {
     if ($scope.current_meal_index < $scope.available_meals.length - 1) {
-      $scope.current_meal_index += 1
+      $scope.current_meal_index += 1;
+      $scope.available_meal = $scope.available_meals[$scope.current_meal_index];
     } else {
       $scope.available_meal = null;
     }
